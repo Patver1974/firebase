b356a0ba952d52111d088d223b6d8157eb617a6f
Patrick <patver1974@gmail.com>
2021-08-21 12:10:50 +0200

    ok read and write

--- app/src/main/java/com/example/exemplefirebase/MainActivity.java
+++ app/src/main/java/com/example/exemplefirebase/MainActivity.java
@@ -10,8 +10,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+
 import com.google.android.play.core.tasks.OnFailureListener;
+import com.google.android.play.core.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,14 +23,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import android.view.View;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-TextView tvread;
+    casseobjet oobj;
+    TextView tvread;
     private DatabaseReference mDatabase;
 Button btwrite,btread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mDatabase = FirebaseDatabase.getInstance("https://exemplefirebase-1423b-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
 tvread = findViewById(R.id.tv_main);
         btwrite = findViewById(R.id.bt_write);
         btread = findViewById(R.id.bt_read);
@@ -46,12 +51,12 @@ btwrite.setOnClickListener(this);
         switch(v.getId()) {
             case R.id.bt_write:
 
-                mDatabase = FirebaseDatabase.getInstance("https://exemplefirebase-1423b-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+
 
                 mDatabase.setValue("Hello, World!");
-                mDatabase.child("users").child("userId").setValue("user");
-                mDatabase.child("users").child("userId5").setValue("user5");
-                mDatabase.child("users").child("userId").setValue("user")
+                mDatabase.child("users").child("userId1").setValue("user11");
+                mDatabase.child("users").child("userId2").setValue("user22");
+                mDatabase.child("users").child("userId3").setValue("user33")
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void unused) {
@@ -62,12 +67,50 @@ btwrite.setOnClickListener(this);
 
                 mDatabase = FirebaseDatabase.getInstance("https://exemplefirebase-1423b-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
 
-                mDatabase.child("users").child("userId5").get().addOnSuccessListener();
+
+
+                break;
+            case R.id.bt_read:
+
+                final String[] userString = new String[1];
+
+                mDatabase.child("users").get()
+                        .addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+                            @Override
+                            public void onSuccess(DataSnapshot dataSnapshot) {
+                               // userString[0] = dataSnapshot.getValue().toString();
+                                String abc = dataSnapshot.child("userId1").getValue().toString();
+                                oobj = new casseobjet (dataSnapshot.child("userId1").getValue().toString(),dataSnapshot.child("userId2").getValue().toString(),dataSnapshot.child("userId3").getValue().toString());
+                                tvread.setText(abc);
+                            }
+                        });
+
+
 
                 break;
 
 
+
+
+
         }
+    }
+
+
+
+    private void finallya() {
+        //case R.id.bt_read:
+
+        final String[] userString = new String[1];
+
+        mDatabase.child("users").get()
+                .addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+                    @Override
+                    public void onSuccess(DataSnapshot dataSnapshot) {
+                        userString[0] = dataSnapshot.getValue().toString();
+                        tvread.setText(userString[0]);
+                    }
+                });
     }
 
 
---
+++ app/src/main/java/com/example/exemplefirebase/casseobjet.java
@@ -0,0 +1,37 @@
+package com.example.exemplefirebase;
+
+public class casseobjet {
+    String un;
+    String deux;
+    String trois;
+
+    public casseobjet(String un, String deux, String trois) {
+        this.un = un;
+        this.deux = deux;
+        this.trois = trois;
+    }
+
+    public String getUn() {
+        return un;
+    }
+
+    public void setUn(String un) {
+        this.un = un;
+    }
+
+    public String getDeux() {
+        return deux;
+    }
+
+    public void setDeux(String deux) {
+        this.deux = deux;
+    }
+
+    public String getTrois() {
+        return trois;
+    }
+
+    public void setTrois(String trois) {
+        this.trois = trois;
+    }
+}
